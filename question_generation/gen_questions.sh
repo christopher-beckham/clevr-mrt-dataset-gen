33251a1696e442a75d82dbdaa5f0a81340688e68
Martin Weiss <martin.clyde.weiss@gmail.com>
2020-02-28 09:43:19 -0500

    update path

--- question_generation/gen_questions.sh
+++ question_generation/gen_questions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python generate_questions.py --template_dir CLEVR_TEXT --instances_per_template 3 --output_questions_file ../output/train/CLEVR_questions.json \
-  --input_scene_file ../output/train/CLEVR_scenes.json
+python generate_questions.py --template_dir CLEVR_TEXT --instances_per_template 3 --output_questions_file ../output/train-clevr-text/CLEVR_questions.json \
+  --input_scene_file ../output/train-clevr-text/CLEVR_scenes.json
 
-python generate_questions.py --template_dir CLEVR_TEXT --instances_per_template 3 --output_questions_file ../output/val/CLEVR_questions.json \
-  --input_scene_file ../output/val/CLEVR_scenes.json
+python generate_questions.py --template_dir CLEVR_TEXT --instances_per_template 3 --output_questions_file ../output/val-clevr-text/CLEVR_questions.json \
+  --input_scene_file ../output/val-clevr-text/CLEVR_scenes.json
 
-python generate_questions.py --template_dir CLEVR_TEXT --instances_per_template 3 --output_questions_file ../output/test/CLEVR_questions.json \
-  --input_scene_file ../output/test/CLEVR_scenes.json
+python generate_questions.py --template_dir CLEVR_TEXT --instances_per_template 3 --output_questions_file ../output/test-clevr-text/CLEVR_questions.json \
+  --input_scene_file ../output/test-clevr-text/CLEVR_scenes.json
 
