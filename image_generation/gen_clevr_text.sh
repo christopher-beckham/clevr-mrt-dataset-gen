56f15a7bdd5e6a5cf0fb284896ef701d8fc79685
Martin Weiss <martin.clyde.weiss@gmail.com>
2020-03-02 18:58:19 -0500

    bump

--- image_generation/gen_clevr_text.sh
+++ image_generation/gen_clevr_text.sh
@@ -9,10 +9,10 @@
 
 # Generate Val set
 ~/.local/bin/blender/blender --background --python render_images.py -- --num_images 32 --use_gpu 1 --max_objects 3 \
---text --all_chars_visible --base_scene_blendfile data/base_scene_tub.blend --enforce_obj_visibility --split train-clevr-text\
+--text --all_chars_visible --base_scene_blendfile data/base_scene_tub.blend --enforce_obj_visibility --split val-clevr-text\
  --output_image_dir ../output/testtest-clevr-text/images/ --output_scene_dir ../output/val-clevr-text/scenes/ --output_scene_file ../output/val-clevr-text/CLEVR_scenes.json
 
 # Generate test set
 ~/.local/bin/blender/blender --background --python render_images.py -- --num_images 32 --use_gpu 1 --max_objects 3 \
---text --all_chars_visible --base_scene_blendfile data/base_scene_tub.blend --enforce_obj_visibility --split train-clevr-text\
+--text --all_chars_visible --base_scene_blendfile data/base_scene_tub.blend --enforce_obj_visibility --split test-clevr-text\
  --output_image_dir ../output/test-clevr-text/images/ --output_scene_dir ../output/test-clevr-text/scenes/ --output_scene_file ../output/test-clevr-text/CLEVR_scenes.json
